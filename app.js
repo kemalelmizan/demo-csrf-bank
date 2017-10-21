426d7e1507bb689a849dd4ff15d69317f726d3d7
kemalelmizan <kemalelmizan@gmail.com>
2017-10-21 21:35:04 +0700

    update helmet

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+// const helmet = require('helmet')
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+// app.use(helmet())
 
 var loggedInUsers = [];
 var userData = [
